ee54b1a3a8107e56d0ef8af36f3f5d9b450e1336
jenschrad <109177708+jenschrad@users.noreply.github.com>
2023-08-29 08:46:01 -0400

    add ex one files

---
+++ ExerciseOne/src/exerciseone/ExerciseOne.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exerciseone;
+
+/**
+ *
+ * @author jenna schrader
+ * this program computes a mathematic expression 
+ * and displays the result to the console
+ */
+
+// begin class
+public class ExerciseOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // begin main 
+    public static void main(String[] args) {
+        // expression in string to display
+        System.out.print("(9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5) = " );
+        // expression to evalutate to display result
+        System.out.println((9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5));
+    }
+    // end main
+    
+}
+// end class
